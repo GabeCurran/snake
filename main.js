a852d10222e4fb23e25d319261c8ccf561391b35
GabeCurran <gabecurran01@gmail.com>
2022-03-27 01:46:23 -0400

    Add Food & Score

--- main.js
+++ main.js
@@ -18,6 +18,10 @@ let snake = [
 let dx = 10;
 let dy = 0;
 
+let score = 0;
+const scoreText = document.querySelector('#score');
+scoreText.textContent = score;
+
 function drawSnakePart(snakePart) {
     board_ctx.fillStyle = snakeColor;
     board_ctx.strokeStyle = snakeBorder;
@@ -38,16 +42,85 @@ function drawSnake() {
 function moveSnake() {
     const head = {x: snake[0].x + dx, y: snake[0].y + dy};
     snake.unshift(head);
-    snake.pop();
+
+    if (head.x == foodX && head.y == foodY) {
+        score++;
+        scoreText.textContent = score;
+        genFood();
+    } else {
+        snake.pop();
+    }
 };
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-};
+function changeDirection(key) {
+    const LEFT = 37;
+    const UP = 38;
+    const RIGHT = 39;
+    const DOWN = 40;
+
+    const keyPress = key.keyCode;
+    const goingLeft = (dx == -10);
+    const goingRight = (dx == 10);
+    const goingUp = (dy == -10);
+    const goingDown = (dy == 10);
+
+    if (keyPress == LEFT && !goingRight) {
+        dx = -10;
+        dy = 0;
+    }
+
+    if (keyPress == RIGHT && !goingLeft) {
+        dx = 10;
+        dy = 0;
+    }
+
+    if (keyPress == UP && !goingDown) {
+        dx = 0;
+        dy = -10;
+    }
+
+    if (keyPress == DOWN && !goingUp) {
+        dx = 0;
+        dy = 10;
+    }
+
+}
+
+function eating(part) {
+    if (part.x == foodX && part.y == foodY) {
+        genFood();
+    }
+}
+
+function makeFood() {
+    apple = new Image();
+    apple.src = 'apple.png';
+    board_ctx.drawImage(apple, foodX, foodY);
+}
+
+function randomFood(min, max) {
+    return Math.round((Math.random() * (max - min) + min) / 10) * 10;
+}
+
+let foodX = randomFood(0, board.width - 10);
+let foodY = randomFood(0, board.height - 10);
+
+function genFood() {
+    foodX = randomFood(0, board.width - 10);
+    foodY = randomFood(0, board.height - 10);
+
+    for (part in snake) {
+        eating(part);
+    }
+}
+
+document.addEventListener('keydown', changeDirection);
+
 
 function main() {
     setTimeout(function onTick() {
         clear();
+        makeFood();
         moveSnake();
         drawSnake();
         main();
